1cdc961ecfea8b8508bdfbadf501f624bf6e25af
Abasin Mohammed <147157387+abas0019@users.noreply.github.com>
2024-10-01 11:01:00 +0200

    oprettet Vehicle.cs

---
+++ TicketClassLibrary/Vehicle.cs
@@ -0,0 +1,27 @@
+namespace TicketClassLibrary;
+
+public abstract class Vehicle
+
+{
+    /// <summary>
+    /// License plate of the vehicle
+    /// </summary>
+    public string Licenseplate { get; set; }
+
+    /// <summary>
+    /// Date of ticket purchase
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Abstract method for price, to be implemented by derived classes
+    /// </summary>
+    /// <returns>Price of the vehicle</returns>
+    public abstract double Price();
+
+    /// <summary>
+    /// Abstract method for vehicle type, to be implemented by derived classes
+    /// </summary>
+    /// <returns>Type of the vehicle</returns>
+    public abstract string VehicleType();
+}
